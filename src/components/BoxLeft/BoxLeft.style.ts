bd43e925c92a22611401ca82f0e3a51679657cb0
Meriam-Chouaib <chouaibmeriem1@gmail.com>
2023-05-02 14:15:40 +0100

    STR-17: make box posts reponsive

--- src/components/BoxLeft/BoxLeft.style.ts
+++ src/components/BoxLeft/BoxLeft.style.ts
@@ -1,6 +1,10 @@
 import { Box, styled } from '@mui/material';
+import theme from '../../theme';
 export const BoxLeft = styled(Box)(() => ({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'flex-start',
+  [theme.breakpoints.down(997)]: {
+    justifyContent: 'center',
+  },
 }));
