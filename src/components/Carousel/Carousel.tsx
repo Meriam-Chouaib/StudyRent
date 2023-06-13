0ed9f7841a40f44a30a3fdf97b94700b8b2023ec
Meriam-Chouaib <chouaibmeriem1@gmail.com>
2023-06-13 13:56:55 +0100

    STR-29: carousel works at first click fixed

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,25 +1,61 @@
-import { useKeenSlider } from 'keen-slider/react';
+// _________________________________ React _______________________________
+import { useEffect, useRef } from 'react';
+
+// _________________________________ KeenSlider _______________________________
+import KeenSlider from 'keen-slider';
 import 'keen-slider/keen-slider.min.css';
+
+// _________________________________ utils _______________________________
 import { getImageSrc } from '../../utils/getDefaultImage';
+
+// _________________________________ mui _______________________________
 import { Box } from '@mui/material';
-import theme from '../../theme';
+
+// _________________________________ styles _______________________________
 import { BoxImages, ImgSlider } from '../../pages/DetailPostPage/DetailPostPage.style';
-interface carouselProps {
+
+interface CarouselProps {
   images?: File[];
 }
-export const Carousel = ({ images }: carouselProps) => {
-  const [ref] = useKeenSlider<HTMLDivElement>({
-    loop: true,
-    mode: 'free',
-    slides: { origin: 'center', perView: 2.5, spacing: 10 },
-    range: {
-      min: -5,
-      max: 5,
-    },
-  });
+
+export const Carousel = ({ images }: CarouselProps) => {
+  const sliderRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let keenSlider: typeof KeenSlider | null = null;
+
+    const initializeSlider = () => {
+      if (sliderRef.current && images && images.length > 0 && !keenSlider) {
+        keenSlider = new KeenSlider(sliderRef.current, {
+          loop: true,
+          mode: 'free',
+          slides: { origin: 'center', perView: 2.5, spacing: 10 },
+          range: {
+            min: -5,
+            max: 5,
+          },
+        });
+      }
+    };
+
+    const destroySlider = () => {
+      if (keenSlider) {
+        keenSlider = null;
+      }
+    };
+
+    initializeSlider();
+    destroySlider();
+    initializeSlider();
+
+    return () => {
+      destroySlider();
+    };
+  }, [images]);
+
   return (
     <>
-      <BoxImages ref={ref} className="keen-slider" height={'20rem'}>
+      <BoxImages ref={sliderRef} className="keen-slider" height={'20rem'}>
         {images?.map((item: File, index) => (
           <Box className={`keen-slider__slide number-slide${index}`} key={index} sx={{}}>
             <ImgSlider src={getImageSrc(item.name)} alt={item.name} key={index} />
